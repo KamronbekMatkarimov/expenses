1070edc67d50097400f05b9ebe12c930466906f6
KamronbekMatkarimov <matkarimovkamron10@gmail.com>
2026-01-28 18:02:49 +0500

    Fix static files on Render (WhiteNoise + collectstatic)

--- config/settings.py
+++ config/settings.py
@@ -31,6 +31,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -113,6 +114,12 @@ REST_FRAMEWORK = {
 
     "EXCEPTION_HANDLER": "config.exception_handler.exception_handler",
 }
+
+STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 
 SPECTACULAR_SETTINGS = {
     "TITLE": "Expenses & Reports API",
