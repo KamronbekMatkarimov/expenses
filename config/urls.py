1478f06bae3b5a7a9bc77f99c119cb390b2133db
KamronbekMatkarimov <matkarimovkamron10@gmail.com>
2026-01-29 17:19:09 +0500

    Add temporary create admin endpoint

--- config/urls.py
+++ config/urls.py
@@ -1,43 +1,27 @@
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 from django.http import HttpResponse
-from django.conf import settings
-from django.conf.urls.static import static
-
-from drf_spectacular.views import (
-    SpectacularAPIView,
-    SpectacularSwaggerView,
-)
-
 from django.contrib.auth import get_user_model
 
+User = get_user_model()
 
 def create_admin(request):
-    User = get_user_model()
+    if User.objects.filter(username="admin").exists():
+        return HttpResponse("Admin already exists")
 
-    username = "admin"
-    password = "admin12345"
-    email = "admin@example.com"
-
-    if not User.objects.filter(username=username).exists():
-        User.objects.create_superuser(
-            username=username,
-            email=email,
-            password=password,
-        )
-        return HttpResponse("Superuser created: admin / admin12345")
-
-    return HttpResponse("Superuser already exists")
-
-
-def healthz(request):
-    return HttpResponse("ok")
-
+    User.objects.create_superuser(
+        username="admin",
+        email="admin@example.com",
+        password="Admin12345!"
+    )
+    return HttpResponse("Admin created successfully")
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+
     path("create-admin/", create_admin),
-    path("healthz/", healthz),
+
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path("api/docs/", SpectacularSwaggerView.as_view(url_name="schema")),
     path("api/users/", include("users.urls")),
@@ -45,10 +29,3 @@ urlpatterns = [
     path("api/budgets/", include("budgets.urls")),
     path("api/reports/", include("reports.urls")),
 ]
-
-
-if settings.DEBUG:
-    urlpatterns += static(
-        settings.MEDIA_URL,
-        document_root=settings.MEDIA_ROOT
-    )
