0d8243487e33bc4a3daa26cd246dc90382faf623
KamronbekMatkarimov <matkarimovkamron10@gmail.com>
2026-01-31 12:16:30 +0500

    reports bug fix

--- reports/urls.py
+++ reports/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import ReportsView, ReportEmailView
 
 urlpatterns = [
-    path("reports/", ReportsView.as_view(), name="reports"),
-    path("reports/email/", ReportEmailView.as_view(), name="reports-email"),
+    path("", ReportsView.as_view(), name="reports"),
+    path("email/", ReportEmailView.as_view(), name="reports-email"),
 ]
